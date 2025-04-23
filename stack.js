32a609e182601fdfec981736de75fc474b1e7faf
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-04-23 08:14:02 -0300

    Add Selector class to select tabs

--- stack.js
+++ stack.js
@@ -22,4 +22,28 @@ class Stack {
   }
 }
 
+class Selector {
+  map = {};
+
+  constructor(elements, selected_id, className) {
+    this.className = className;
+    for (const child of elements) {
+      const id = child.id;
+      this.map[id] = child;
+      if (id == selected_id) {
+        child.classList.add(className);
+      }
+    }
+    this.selected_id = selected_id;
+  }
+
+  select(id) {
+    this.map[this.selected_id].classList.remove(this.className);
+    this.map[id].classList.add(this.className);;
+    this.selected_id = id;
+  }
+}
+
+// TODO: Remove stack in favour of Selector
 const stack = new Stack(document.getElementById("stack"), "proposal");
+const selector = new Selector(document.querySelectorAll('nav > button'), "tab-proposal", "tab-selected");
