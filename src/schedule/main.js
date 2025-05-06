1432a54179e3d3a44d65e853ade9d3c893a31ad8
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-06 01:01:50 -0300

    schedule: Add initial reports

--- src/schedule/main.js
+++ src/schedule/main.js
@@ -1,6 +1,8 @@
 import inject from "../lib/inject";
-import shortTermHTML from "./short-term.html?raw";
 import longTermHTML from "./long-term.html?raw";
+import shortTermHTML from "./short-term.html?raw";
+import reportsHTML from "./reports.html?raw";
 
 inject("#schedule-long-term", longTermHTML);
 inject("#schedule-short-term", shortTermHTML);
+inject("#schedule-reports", reportsHTML);
