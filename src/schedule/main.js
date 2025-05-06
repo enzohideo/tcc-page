0058a4cde190be6892c561d695d1d5cf21321e51
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-06 12:30:23 -0300

    schedule: Separate index.html and rename Reports to Progress

--- src/schedule/main.js
+++ src/schedule/main.js
@@ -1,8 +1,11 @@
 import inject from "../lib/inject";
+
+import scheduleHTML from "./index.html?raw";
 import longTermHTML from "./long-term.html?raw";
 import shortTermHTML from "./short-term.html?raw";
-import reportsHTML from "./reports.html?raw";
+import progressHTML from "./progress.html?raw";
 
+inject("#schedule", scheduleHTML);
 inject("#schedule-long-term", longTermHTML);
 inject("#schedule-short-term", shortTermHTML);
-inject("#schedule-reports", reportsHTML);
+inject("#schedule-progress", progressHTML);
