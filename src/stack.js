15cd7ab9024dd7cdf2ee7d98cb372290d2e678e7
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-05 23:31:35 -0300

    script: Separate stack.js and main.js

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-class Stack {
+export class Stack {
   hide = "display: none;";
   show = "";
   map = {}; // I known... TODO: Use Map
@@ -22,7 +22,7 @@ class Stack {
   }
 }
 
-class Selector {
+export class Selector {
   map = {};
 
   constructor(elements, selected_id, className) {
@@ -43,41 +43,3 @@ class Selector {
     this.selected_id = id;
   }
 }
-
-const searchParams = new URL(window.location.href).searchParams;
-const startingTab = searchParams.get("tab") || "proposal";
-
-// TODO: Remove stack in favour of Selector
-const stack = new Stack(document.getElementById("stack"), startingTab);
-const selector = new Selector(
-  document.querySelectorAll("nav > button"),
-  `tab-proposal`,
-  "tab-selected",
-);
-
-document.getElementById("tab-proposal").onclick = () => {
-  stack.view("proposal");
-  selector.select("tab-proposal");
-};
-
-document.getElementById("tab-schedule").onclick = () => {
-  stack.view("schedule");
-  selector.select("tab-schedule");
-
-  document.querySelectorAll("#schedule iframe").forEach((iframe) => {
-    iframe.style.height = `${iframe.contentWindow.document.documentElement.scrollHeight}px`;
-  });
-};
-
-document.getElementById("tab-commits").onclick = () => {
-  stack.view("commits");
-  selector.select("tab-commits");
-};
-
-document.getElementById("tab-monograph").onclick = () => {
-  stack.view("monograph");
-  selector.select("tab-monograph");
-};
-
-const startingTabElm = document.getElementById(`tab-${startingTab}`);
-window.addEventListener("load", startingTabElm.onclick);
