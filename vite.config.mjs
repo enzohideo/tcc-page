eb4f505d2c5115c4d94f7fe9179ef6e9fbdf4428
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-17 20:00:26 -0300

    chore: Add support for shell highlighting

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   plugins: [
     prism({
-      languages: ["javascript", "json"],
+      languages: ["javascript", "json", "shell"],
       plugins: ["line-numbers"],
       theme: "tomorrow",
       css: true,
